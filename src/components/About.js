5792599ea0037ea7c6eb0c3a28bd2e834eb8b2a6
Felix Muwanguzi <felixm@Felixs-MacBook-Air.local>
2021-01-17 21:14:29 -0500

    [components] about and footer

--- src/components/About.js
+++ src/components/About.js
@@ -2,15 +2,14 @@ import React from 'react';
 
 const About = () => {
     return(
-        <> 
-        
+        <div>
         <h1>About us</h1>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                Tempore dolorum excepturi, pariatur dolore fuga perferendis 
-                mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
-                dolorem tempora possimus accusantium, fugit eius quidem?
+            Tempore dolorum excepturi, pariatur dolore fuga perferendis 
+            mollitia deserunt a voluptas assumenda! Consequatur beatae qui 
+            dolorem tempora possimus accusantium, fugit eius quidem?
         </p>
-        </>
+        </div>
     )
 }
 
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,18 +2,13 @@ import React from 'react';
 
 const Footer = () => {
     return(
-        <footer>
-            <div>
-                <span>
-                    @2020 built be Felix M 
+        <footer className="footer bg-dark">
+            <div className="container text-center">
+                <span className="text-muted">
+                    @2021 built by Felix M (during COVID) 
                 </span>
             </div>
-
-
-        </footer>
-        
-    
-        
+        </footer>    
     )
 }
 
